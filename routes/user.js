d4a0bfc385253b1b649ac8b2e0815e5c8dbba72f
Affaq-Ahmed <affaqahmed165@gmail.com>
2022-06-14 21:44:39 +0500

    Change Preference

--- routes/user.js
+++ routes/user.js
@@ -23,11 +23,11 @@ router.post("/createProfile", async (req, res) => {
 	console.log(req.body);
 	try {
 		const check = await user.doc(req.body.uId).get();
-		if (check.exists) res.status(409).json("User Already Exists.");
+		if (check) res.status(409).json("User Already Exists.");
 		console.log(check);
 
 		const checkCNIC = await user.where("cnic", "==", req.body.cnic).get();
-		if (!checkCNIC.empty) res.status(409).json("CNIC Already Exists.");
+		if (!checkCNIC) res.status(409).json("CNIC Already Exists.");
 		console.log(checkCNIC);
 
 		const data = req.body;
@@ -62,16 +62,11 @@ router.post("/createProfile", async (req, res) => {
 router.post("/editProfile", async (req, res) => {
 	try {
 		const check = user.doc(req.body.uId).get();
-		if (!check.exists) res.status(409).json("User Not Found.");
+		if (!check) res.status(409).json("User Not Found.");
 		else {
 			const data = req.body;
-
 			const updatedUser = await user.doc(data.uId).update({
-				firstName: data.firstName,
-				lastName: data.lastName,
-				address: data.address,
-				profileImage: data.profileImage,
-				phone: data.phone,
+				data,
 			});
 
 			console.log(updatedUser);
