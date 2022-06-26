b7a42c5f933174d75726de94891d6b4b0425e23d
Affaq-Ahmed <affaqahmed165@gmail.com>
2022-06-26 16:51:37 +0500

    Reports added

--- routes/report.js
+++ routes/report.js
@@ -3,21 +3,34 @@ const router = express.Router();
 const { db } = require("../config/firebase");
 
 const report = db.collection("report");
+const order = db.collection("order");
+const user = db.collection("users");
 
 //CREATE A REPORT
 router.post("/", async (req, res) => {
+	const { orderId, reportMessage, reporterId } = req.body;
 	try {
-		const data = {
-			orderId: req.body.orderId,
-			reporterId: req.body.reporterId,
-			reportMessage: req.body.reportMessage,
-			createdOn: new Date(),
-		};
+		const checkOrder = await order.doc(orderId).get();
+		if (checkOrder.exists) {
+			const checkReporter = await user.doc(reporterId).get();
+			if (checkReporter.exists) {
+				const data = {
+					orderId: orderId,
+					reporterId: reporterId,
+					reportMessage: reportMessage,
+					createdOn: new Date(),
+				};
 
-		const reportRef = await report.add(data);
-		console.log(reportRef);
+				const reportRef = await report.add(data);
+				console.log(reportRef);
 
-		res.status(200).json("Report added successfully");
+				res.status(200).json("Report added successfully");
+			} else {
+				res.status(200).json("Reporter not found");
+			}
+		} else {
+			res.status(200).json("Order not found");
+		}
 	} catch (error) {
 		console.log(error);
 		res.status(500).send(error);
