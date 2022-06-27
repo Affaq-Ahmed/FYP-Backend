578bd8260bb3fb8eab599b502e994a72188623bd
Affaq-Ahmed <affaqahmed165@gmail.com>
2022-06-27 11:31:38 +0500

    Working on Admin

--- routes/order.js
+++ routes/order.js
@@ -219,9 +219,10 @@ router.put("/completeOrder", async (req, res) => {
 				});
 
 				//UPDATE EARNINGS OF THE SELLER
+				const newBalance = resultOrder.data().price * 0.9;
 				const userRef = await user.doc(resultOrder.data().sellerId).update({
 					earnings: FieldValue.increment(resultOrder.data().price),
-					balance: FieldValue.increment(resultOrder.data().price * 0.9),
+					balance: FieldValue.increment(parseInt(newBalance)),
 				});
 
 				res.status(200).json("Order Completed.");
